1d59d111d5da0d91659e781ffba22b6bc07b6d4e
Marslel <marcellino.janitra@yahoo.com>
2021-01-13 20:55:36 +0700

    table class fix heroku

--- database/migrations/2021_01_13_134820_create_pekobs_table.php
+++ database/migrations/2021_01_13_134820_create_pekobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Pekobs extends Migration
+class CreatePekobsTable extends Migration
 {
     /**
      * Run the migrations.
