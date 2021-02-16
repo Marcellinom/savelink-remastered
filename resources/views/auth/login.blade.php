adbe1895bf2e8cd1f934099d4a7cd93d811d2a15
Marslel <marcellino.janitra@yahoo.com>
2021-02-17 02:08:12 +0700

    login feature, general improvement

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,54 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <img src="{{asset('img/logo.png')}}" width="300px">
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="email" class="text-sm text-white" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" class="text-sm text-white" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-white">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+            
+
+            <div>
+                @if (Route::has('password.request'))
+                <a class="text-sm text-white hover:text-red-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+                @endif
+                <a href="{{ route('register') }}" style="float: right" class="text-sm text-white">
+                    Don't have an account?
+                </a>
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                
+                <x-jet-button class="ml-4">
+                    {{ __('Login') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
