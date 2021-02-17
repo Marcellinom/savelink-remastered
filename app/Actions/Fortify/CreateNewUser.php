3d660c8885ddbb540b74d0213d6cec85e1bec274
Marslel <marcellino.janitra@yahoo.com>
2021-02-17 19:09:59 +0700

    debuggind

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,7 +21,6 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input)
     {
-        return dd($input);
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
