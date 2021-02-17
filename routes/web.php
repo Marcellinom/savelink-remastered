611f2c7761e19306d0b160898044beb66e3390a7
Marslel <marcellino.janitra@yahoo.com>
2021-02-17 17:53:29 +0700

    adding middleware web

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,18 @@ use App\View\Components\AppLayout;
 */
 
 
-
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('/', function () {
-        return redirect()->route('home');
+Route::group(['middleware' => 'web'], function () {
+    Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+        Route::get('/', function () {
+            return redirect()->route('home');
+        });
+        Route::get('/home', [homeController::class, 'index'])->name('home');
+        Route::get('/account', [homeController::class, 'account'])->name('account');
+        Route::get('view/{tag}', [homeController::class,'view'])->name('view');
+        Route::post('input', [inputController::Class,'input_table']);
+        Route::post('/addTag', [homeController::class, 'addTag'])->name('addTag');
+        Route::post('/delete', [inputController::Class,'delete'])->name('delete');
+        Route::post('/edit', [inputController::Class,'edit'])->name('edit');
+        Route::post('/purge', [inputController::Class,'purge'])->name('purge');
     });
-    Route::get('/home', [homeController::class, 'index'])->name('home');
-    Route::get('/account', [homeController::class, 'account'])->name('account');
-    Route::get('view/{tag}', [homeController::class,'view'])->name('view');
-    Route::post('input', [inputController::Class,'input_table']);
-    Route::post('/addTag', [homeController::class, 'addTag'])->name('addTag');
-    Route::post('/delete', [inputController::Class,'delete'])->name('delete');
-    Route::post('/edit', [inputController::Class,'edit'])->name('edit');
-    Route::post('/purge', [inputController::Class,'purge'])->name('purge');
 });
